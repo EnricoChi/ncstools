2a79205516b501eb44635e557e009a2c9535d4f1
ealesid <ealesid@gmail.com>
2019-01-22 19:43:40 +0300

    projects dialog - floating buttons

--- vueApp/components/appDialogProjects.vue
+++ vueApp/components/appDialogProjects.vue
@@ -7,15 +7,30 @@
     scrollable
   >
     <v-card tile>
+      
       <v-toolbar card dark color="primary">
+        
         <v-btn icon dark @click="toggleDialog(dialog.name)">
           <v-icon>fa-angle-double-left</v-icon>
         </v-btn>
+        
+        <v-spacer></v-spacer>
         <v-toolbar-title>Projects</v-toolbar-title>
         <v-spacer></v-spacer>
+
+        <v-toolbar-items class="fab-container">
+          <v-btn fab small color="teal accent-3">
+            <v-icon>fa-plus</v-icon>
+          </v-btn>
+          <v-btn fab small color="deep-orange accent-3">
+            <v-icon>fa-refresh</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+        
       </v-toolbar>
+      
+      
       <v-card-text>
-        <v-btn color="primary" dark>Open Dialog 2</v-btn>
 
         <v-list three-line subheader>
           <v-subheader>User Controls</v-subheader>
@@ -61,5 +76,9 @@
 </script>
 
 <style scoped>
-
+  .fab-container {
+    position: fixed;
+    top: 30px;
+    left: 40px;
+  }
 </style>
